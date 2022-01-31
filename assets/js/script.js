ee3cca409be3ef5f49c6689df433042971243007
Aimee Truong <aimtruong@ucdavis.edu>
2022-01-30 23:24:56 -0800

    localstorage and eventcolor still left

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,64 @@
 
 // current Date in header
 var currentDate = $("#currentDay");
-    currentDate = moment(currentDate).format("dddd, MMMM Do");
+currentDate = moment(currentDate).format("dddd, MMMM Do");
 
 // header to append current Date
 var header = $("header");
-    header.append(currentDate);
+header.append(currentDate);
 
-const container = $(".container");
+var container = $(".container");
 
-const now = moment();
+var now = moment();
 
-const currentTime = {text: moment().format("h:00 A"), hour: moment().hour()};
+// credits for function layout to https://codepen.io/judebloom/pen/RwGbVWB
+var currentTime = {text: moment().format("hA"), hour: moment().hour()};
 
-const range = (start, end, step) => {
-  return Array.from(
-    Array.from(Array(Math.ceil((end - start) / step)).keys()),
-    (x) => start + x * step
-  );
-};
-
-const hoursOfTheDay = Array.from(new Array(9)).map((v, i) => {
-  const text = moment().hour(i).format("hA");
-  const hour = moment().hour(i);
+var hoursOfTheDay = Array.from(new Array(9)).map((v, i) => {
+  var text = moment().hour(i).format("hA");
+  var hour = moment().hour(i);
   return {text, hour};
 });
 
+var text = $(this).siblings(".description").val();
+var time = $(this).parent().attr("id");
+
+function color() {
+  return time === currentTime.text
+    ? "past"
+    : time < now
+    ? "present"
+    : "future";
+}
 
 hoursOfTheDay.forEach((hr) => {
-  const eventForm = $(`<form data-name = "${hr.text}" class = "row row-cols-3 time-block"></form>`);
 
-  const time = $(`<div class = "hour col d-flex align-items-center justify-content-center">${hr.text}</div>`);
-
-  const textArea = $(`<textarea name = "${hr.text}" class = "description col col-9 ${color(hr)}">${window.localStorage.getItem(hr.text) || ""}</textarea>`);
-
-  textArea.keydown((e) => {
-    if (e.keyCode == 8 && !e.shiftKey) {
-      e.preventDefault();
-      return false;
-    }
-  });
-
-  
-  const saveButton = $("<button type = 'submit' class = 'col d-flex align-items-center justify-content-center rounded'><span class = 'oi oi-box'></span></button>");
+  var textArea = document.querySelectorAll("textarea");
+    textArea.addClass(color(hr));
 
   eventForm.submit((e) => {
     e.preventDefault();
 
-    const value = $("textarea[name='${hr.text}']").val();
 
-    window.localStorage.setItem(hr.text, value);
-  });
-
-  
-  function color() {
+    //set items in local storage.
+    localStorage.setItem(time, text);
     
-    return time.text === currentTime.text
-      ? "past"
-      : time.hour < now
-      ? "present"
-      : "future";
-  }
-
-  
-  eventForm.append(time);
-  eventForm.append(textArea);
-  eventForm.append(saveButton);
-
-  container.append(eventForm);
+  });
 });
+
+
+function loadEvents(){
+  if (storedEvents) {
+    for(var i = 0; i < 9; i++){
+      Events.hour = storedEvents;
+    }
+  }
+  if(!storedEvents){
+    for(var i = 0; i < 9; i++){
+      Events[i] = "";
+    }
+  }
+};
+
+
+loadEvents();
